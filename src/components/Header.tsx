dc792cc8c010a78e002653bc0697133a567f3aba
William <williampo78@gmail.com>
2024-08-17 10:58:18 +0800

    調整layout、編輯群組成員

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
-import { Link } from 'react-router-dom';
-import { useStore } from '@/stores/index';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useStore } from "@/stores/index";
+import { FaChevronLeft } from "react-icons/fa6";
 
 function Header() {
-	const headerTitle = useStore((state) => state.headerTitle);
+  const headerTitle = useStore((state) => state.headerTitle);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	return (
-		<>
-			<header className="bg-sage-200 w-full px-4 py-2 text-white text-center text-3xl">
-				<Link to="/">{headerTitle || '分帳趣'}</Link>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header className="bg-sage-200 w-full px-4 py-2 text-white text-center text-3xl relative">
+        {location.pathname !== "/" && (
+          <button
+            onClick={() => {
+              navigate(-1);
+            }}
+            className="absolute left-2 top-1/2 -translate-y-1/2"
+          >
+            <FaChevronLeft />
+          </button>
+        )}
+        <Link to="/">{headerTitle || "分帳趣"}</Link>
+      </header>
+    </>
+  );
 }
 
 export default Header;
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,58 +1,96 @@
-import { useEffect, useState } from 'react';
-import { getBillsAPi } from '@/api/bill';
-import { useParams } from 'react-router-dom';
-import { AiOutlineUsergroupAdd, AiOutlinePlus, AiTwotoneEdit } from 'react-icons/ai';
-import { useStore } from '@/stores/index';
-interface Bill {
-	_id: string;
-	item: string;
-	groupId: string;
-	createdAt: string;
-	updatedAt: string;
+import { useEffect, useState } from "react";
+import { getGroupUsersApi, updateGroupUsersApi } from "@/api/user";
+// import { useParams } from "react-router-dom";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useStore } from "@/stores/index";
+interface User {
+  _id?: string;
+  name: string;
 }
 function Users() {
-	const { setHeaderTitle } = useStore();
+  const { setHeaderTitle } = useStore();
 
-	let { code } = useParams();
-	const [bills, setBills] = useState<Bill[]>([]);
+  //   const { code } = useParams();
+  const [users, setUsers] = useState<User[]>([]);
 
-	useEffect(() => {
-    setHeaderTitle('編輯成員' || '');
+  useEffect(() => {
+    setHeaderTitle("編輯成員" || "");
+    getUsers();
+  }, []);
 
-	}, []);
+  const getUsers = async () => {
+    const { data } = await getGroupUsersApi("669d0457539ce90562ac344f");
+    if (data?.length) {
+      setUsers(data as User[]);
+    } else {
+      setUsers([{ name: "" }]);
+    }
+    console.log(data);
+  };
 
-	const getBills = async () => {
-		const { data } = await getBillsAPi({ code: code as string });
-		setBills((data.bills as Bill[]) || []);
+  const updateUsers = async () => {
+    const data = await updateGroupUsersApi("669d0457539ce90562ac344f", {
+      users: users,
+    });
+    console.log(data);
+  };
 
-	};
-
-	return (
-		<div className="max-w-[900px] mx-auto">
-            <h1>Users page</h1>
-			{/* <ul className="flex flex-col gap-2">
-				{bills.map((bill, index) => (
-					<li
-						key={index}
-						className="p-3 border-2 border-gray-400 bg-white rounded-md overflow-hidden flex  justify-between items-center"
-					>
-						<span className="text-lg">{bill.item}</span>
-						<button>
-							<AiTwotoneEdit className="w-6 h-6 text-gray-400" />
-						</button>
-					</li>
-				))}
-			</ul> */}
-			<div className="fixed right-8 bottom-24">
-				<button className="w-12 h-12 text-white rounded-full bg-sage-500 flex justify-center items-center">
-					<AiOutlineUsergroupAdd className="w-8 h-8" />
-				</button>
-				<button className="mt-2 w-12 h-12 text-white rounded-full bg-sage-300 flex justify-center items-center">
-					<AiOutlinePlus className="w-8 h-8" />
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="max-w-[900px] mx-auto">
+      <ul className="flex flex-col gap-2">
+        {users.map((user, index) => (
+          <li
+            key={index}
+            className="px-3 py-2 border-2 border-gray-400 bg-white rounded-md overflow-hidden flex justify-between items-center"
+          >
+            <input
+              className="flex-1"
+              type="text"
+              value={user.name}
+              onChange={(e) => {
+                const copiedUsers = [...users];
+                copiedUsers[index].name = e.target.value;
+                setUsers(copiedUsers);
+              }}
+            />
+            {users.length > 1 && (
+              <button
+                onClick={() => {
+                  const copiedUsers = [...users];
+                  setUsers(copiedUsers.filter((_, i) => i !== index));
+                }}
+                className="btn bg-red-600 "
+              >
+                <FaRegTrashAlt />
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div className="flex justify-center mt-6 gap-2">
+        <button
+          onClick={() => {
+            const copiedUsers = [...users];
+            if (copiedUsers.at(-1)?.name) {
+              copiedUsers.push({ name: "" });
+              setUsers(copiedUsers);
+            }
+          }}
+          className="btn bg-teal-300"
+        >
+          增加成員
+        </button>
+        <button
+          onClick={() => {
+            updateUsers();
+          }}
+          className="btn bg-sage-300 "
+        >
+          儲存
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Users;
