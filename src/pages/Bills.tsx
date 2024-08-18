3496e511528fa2dc143bf9fe0dc78f5564dd6d7f
unknown <williampo78@gmail.com>
2024-08-18 13:31:43 +0800

    調整footer

--- src/pages/Bills.tsx
+++ src/pages/Bills.tsx
@@ -1,50 +1,50 @@
-import { useEffect, useState } from "react";
-import { getBillsAPi } from "@/api/bill";
-import { useParams, useNavigate } from "react-router-dom";
-import { AiTwotoneEdit } from "react-icons/ai";
-import { useStore } from "@/stores/index";
+import { useEffect, useState } from 'react';
+import { getBillsAPi } from '@/api/bill';
+import { useParams, useNavigate } from 'react-router-dom';
+import { AiTwotoneEdit } from 'react-icons/ai';
+import { useStore } from '@/stores/index';
 interface Bill {
-  _id: string;
-  item: string;
-  groupId: string;
-  createdAt: string;
-  updatedAt: string;
+	_id: string;
+	item: string;
+	groupId: string;
+	createdAt: string;
+	updatedAt: string;
 }
 function Bills() {
-  const { setHeaderTitle } = useStore();
+	const { setHeaderTitle } = useStore();
 
-  const { code } = useParams();
-  console.log(code);
-  
-  const [bills, setBills] = useState<Bill[]>([]);
+	const { code } = useParams();
+	console.log(code);
 
-  useEffect(() => {
-    getBills();
-  }, []);
+	const [bills, setBills] = useState<Bill[]>([]);
 
-  const getBills = async () => {
-    const { data } = await getBillsAPi({ code: code as string });
-    setBills((data.bills as Bill[]) || []);
-    setHeaderTitle(data.headerTitle || "");
-  };
+	useEffect(() => {
+		getBills();
+	}, []);
 
-  return (
-    <div className="max-w-[900px] mx-auto">
-      <ul className="flex flex-col gap-2">
-        {bills.map((bill, index) => (
-          <li
-            key={index}
-            className="p-3 border-2 border-gray-400 bg-white rounded-md overflow-hidden flex  justify-between items-center"
-          >
-            <span className="text-lg">{bill.item}</span>
-            <button>
-              <AiTwotoneEdit className="w-6 h-6 text-gray-400" />
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	const getBills = async () => {
+		const { data } = await getBillsAPi({ code: code as string });
+		setBills((data.bills as Bill[]) || []);
+		setHeaderTitle(data.groupName || '');
+	};
+
+	return (
+		<div className="max-w-[900px] mx-auto">
+			<ul className="flex flex-col gap-2">
+				{bills.map((bill, index) => (
+					<li
+						key={index}
+						className="p-3 border-2 border-gray-400 bg-white rounded-md overflow-hidden flex  justify-between items-center"
+					>
+						<span className="text-lg">{bill.item}</span>
+						<button>
+							<AiTwotoneEdit className="w-6 h-6 text-gray-400" />
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default Bills;
