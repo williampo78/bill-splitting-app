6d3c44ba86f1d896c3ca79e98ff1f34229c09956
William <williampo78@gmail.com>
2024-11-10 11:55:20 +0800

    調整header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,30 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useStore } from "@/stores/index";
-import { FaChevronLeft } from "react-icons/fa6";
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useStore } from '@/stores/index';
+import { FaChevronLeft } from 'react-icons/fa6';
 
 function Header() {
-  const headerTitle = useStore((state) => state.headerTitle);
-  const navigate = useNavigate();
-  const location = useLocation();
+	// const headerTitle = useStore((state) => state.headerTitle);
+	const { headerTitle, groupInfo } = useStore();
+	const navigate = useNavigate();
+	const location = useLocation();
 
-  return (
-    <>
-      <header className="bg-sage-200 w-full px-4 py-2 text-white text-center text-xl  sticky top-0">
-        {location.pathname !== "/" && (
-          <button
-            onClick={() => {
-              navigate(-1);
-            }}
-            className="absolute left-2 top-1/2 -translate-y-1/2"
-          >
-            <FaChevronLeft />
-          </button>
-        )}
-        <Link to="/">{headerTitle || "分帳趣"}</Link>
-      </header>
-    </>
-  );
+	return (
+		<>
+			<header className="bg-sage-200 w-full px-4 py-2 text-white text-center text-xl  sticky top-0">
+				{location.pathname !== '/' && (
+					<button
+						onClick={() => {
+							navigate(-1);
+						}}
+						className="absolute left-2 top-1/2 -translate-y-1/2"
+					>
+						<FaChevronLeft />
+					</button>
+				)}
+				<Link to={`/group/${groupInfo.code}/bills`}>{headerTitle || '分帳趣'}</Link>
+			</header>
+		</>
+	);
 }
 
 export default Header;
